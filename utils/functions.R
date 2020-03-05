19710340f3751e87216db476fbe122604dbfddc3
Dominick J. Lemas <dominicklemas@gmail.com>
2020-03-04 21:05:04 -0500

    function started

---
+++ utils/functions.R
@@ -0,0 +1,13 @@
+# useful functions
+
+data_inventory=function(df,group_var,event,question,response)
+                {df %>%
+                group_by(!! group_var) %>%
+                filter(study_visit==event) %>%
+                select(group_var,event,question,response) %>%
+                drop_na(response) %>%
+                summarize(count=length(response)) %>%
+                mutate(percent=(count/max(count)*100)) %>%
+                mutate(study_visit=event) %>%
+                select(group_var,event, everything())
+                } # end function
