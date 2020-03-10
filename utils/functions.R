553ccf6f100fc6a972a5ff96adf1d87cd7866466
Dominick J. Lemas <dominicklemas@gmail.com>
2020-03-10 19:04:34 -0400

    utils/data_inventory added

--- utils/functions.R
+++ utils/functions.R
@@ -1,16 +1,25 @@
 # useful functions
 
-data_inventory=function(df,group_var,event,question,response)
-                {df %>%
-                group_by(!! group_var) %>%
-                dplyr::filter(df, study_visit== event) 
-} # end function
-  
-  %>%
-                select(group_var,event,question,response) %>%
-                drop_na(response) %>%
-                summarize(count=length(response)) %>%
-                mutate(percent=(count/max(count)*100)) %>%
-                mutate(study_visit=event) %>%
-                select(group_var,event, everything())
-                } # end function
+# data inventory(): 
+data_inventory <- function(data = NULL, group_var, row_match, col_match, question,response){
+  if(!missing(data)) {
+    
+    #Variable create
+    col <- sym(col_match)
+    row <- row_match
+    group <-sym(group_var)
+    
+    # function 
+    matching <- data %>%
+      filter(!!col == row) %>%
+      select(test_id,study_visit,question,response) %>%
+      drop_na(!! response) %>%
+      group_by(!! group) %>%
+      summarize(count=length(response)) %>%
+      mutate(percent=(count/max(count)*100)) %>%
+      mutate(study_visit=row) %>%
+      select(test_id,study_visit, everything())
+  } else {
+    message("You must specify data frame!")
+  }
+} # END FUNCTION
